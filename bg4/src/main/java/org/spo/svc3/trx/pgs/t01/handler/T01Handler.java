d5217fd1463107d3ef85ac4d76899e7cd971a9f4
pg <misterprem@gmail.com>
2018-06-12 19:10:18 +0530

    fx

--- bg4/src/main/java/org/spo/svc3/trx/pgs/t01/handler/T01Handler.java
+++ bg4/src/main/java/org/spo/svc3/trx/pgs/t01/handler/T01Handler.java
@@ -2,9 +2,10 @@ package org.spo.svc3.trx.pgs.t01.handler;
 
 import org.spo.ifs3.dsl.controller.AbstractHandler;
 import org.spo.ifs3.dsl.controller.DSLConstants.EventType;
+import org.spo.ifs3.dsl.controller.NavEvent;
 import org.spo.svc3.trx.pgs.t01.task.T0101;
 import org.spo.svc3.trx.pgs.t01.task.T0102;
-import org.spo.ifs3.dsl.controller.NavEvent;
+import org.spo.svc3.trx.pgs.t01.task.T0103;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,11 +20,19 @@ public class T01Handler extends AbstractHandler{
    @Autowired
    T0102 t0102;
    
+   @Autowired
+   T0103 t0103;
+   
    public static final NavEvent EV_INIT_01 =  NavEvent.create(EventType.REFRESHPAGE);
    public static final NavEvent EV_INIT_02 =  NavEvent.create(EventType.REFRESHPAGE, "c01/C0102");
+   public static final NavEvent EV_INIT_03 =  NavEvent.create(EventType.REFRESHPAGE);
    
-   public static final NavEvent EV_SWITCH_TO_DTL =  NavEvent.create(EventType.TASKSET, "02");
+   public static final NavEvent EV_SWITCH_TO_DTL =  NavEvent.create(EventType.TASKSET, "02");   
+   public static final NavEvent EV_SWITCH_TO_INST =  NavEvent.create(EventType.TASKSET, "03");
+   
    public static final NavEvent EV_PVS =  NavEvent.create(EventType.TASKSET, "01");
+   public static final NavEvent EV_PVS_01 =  NavEvent.create(EventType.TASKSET, "01");
+   
    
    public static final NavEvent EV_REFRESH_CONTENT =  NavEvent.create(EventType.TASKSET, "01");
    public static final NavEvent EV_REFRESH_NEW_SUB_LAND =  NavEvent.create(EventType.TRXSWITCH, "C01");
@@ -34,6 +43,7 @@ public class T01Handler extends AbstractHandler{
    public void configureChannel() {
 			taskChannel.put("01",t0101);
 			taskChannel.put("02",t0102);
+			taskChannel.put("03",t0103);
    }
    
    
--- bg4/src/main/java/org/spo/svc3/trx/pgs/t01/task/T0103.java
+++ bg4/src/main/java/org/spo/svc3/trx/pgs/t01/task/T0103.java
@@ -2,7 +2,10 @@ package org.spo.svc3.trx.pgs.t01.task;
 
 import java.lang.reflect.Type;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spo.cms3.svc.PageService;
+import org.spo.cms3.svc.SocketConnector;
 import org.spo.ifs3.dsl.controller.NavEvent;
 import org.spo.ifs3.dsl.controller.TrxInfo;
 import org.spo.ifs3.dsl.model.AbstractTask;
@@ -15,19 +18,21 @@ import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
 @Component
-public class T0101 extends AbstractTask {
+public class T0103 extends AbstractTask {
+
+
+
+	private static final Logger logger = LoggerFactory.getLogger(T0103.class);
+	
 	@Autowired
 	public PageService svc ;
 	
+	private SocketConnector connector=new SocketConnector();
+	
 	@Override
-	public NavEvent initView(TrxInfo info) {
-		System.out.println("in init view");
-		return null;
-	}
-
-	@Override
-	public NavEvent initTask(String dataId, TrxInfo info) throws Exception {
+	public NavEvent initTask(String dataId, TrxInfo info) {
 
 
 		String dataId_Content= dataId;//Here dataId represents the template incoming because it is mostly a set of links in this page wiht a small intro
@@ -38,25 +43,20 @@ public class T0101 extends AbstractTask {
 		Gson gson = new Gson();
 		
 		Type typ = new TypeToken<Home_pg>(){}.getType();//FIXME right now only string works
-		Home_pg cmd_menu= gson.fromJson(response,typ);	
 		
 		try{
-			response_content = svc.readUpPage("posts", dataId_Content);
+			response_content = svc.readUpPage("posts", "Wel_msg_inst");
 			typ = new TypeToken<Wel_msg>(){}.getType();//FIXME right now only string works
 			Wel_msg cmd= gson.fromJson(response_content,typ);		
-			
-			info.addToModelMap("menu",cmd_menu);
 			info.addToModelMap("message",cmd);
-			info.put(T01Toolkit.SV_T01_CONTENT_OVV, cmd);
-			info.put(T01Toolkit.SV_T01_MENU_OVV, cmd_menu);
 			System.out.println(cmd.toString());
-
+			info.put(T01Toolkit.SV_T02_CONTENT_OVV, cmd);
 		}catch(Exception e){
 			System.out.println("Error during messagePayload processing from  TestResourceServerException on" );
 			e.printStackTrace();
 		}
 
-		return T01Handler.EV_INIT_01;
+		return T01Handler.EV_INIT_03;
 	
 	}
 
@@ -64,12 +64,12 @@ public class T0101 extends AbstractTask {
 	public NavEvent processViewEvent(String event, TrxInfo info) {
 		if(event.startsWith("EV_DTL")){
 			String dataId = event.replaceAll("EV_DTL_","");
-			NavEvent navEvent = T01Handler.EV_SWITCH_TO_DTL;
+			NavEvent navEvent = T01Handler.EV_REFRESH_CONTENT;
 			navEvent.dataId=dataId;
 			return navEvent;
-		}else if(event.startsWith("EV_INST")){
-			String dataId = event.replaceAll("EV_DTL_","");
-			NavEvent navEvent = T01Handler.EV_SWITCH_TO_INST;
+		}if(event.startsWith("EV_PVS")){
+			String dataId = "Wel_msg";
+			NavEvent navEvent = T01Handler.EV_PVS_01;
 			navEvent.dataId=dataId;
 			return navEvent;
 		}else if(event.startsWith("EV_SUB_LAND")){
@@ -81,19 +81,34 @@ public class T0101 extends AbstractTask {
 		return T01Handler.EV_SWITCH_TO_OVV;
 	}
 
+	
 	@Override
 	public NavEvent processViewResult(String event, String json, TrxInfo info) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
+	
 	@Override
 	public String processAjaxEvent(String event, TrxInfo info) {
+		return "";
+	}
+
+	public SocketConnector getConnector() {
+		return connector;
+	}
+
+	public void setConnector(SocketConnector connector) {
+		this.connector = connector;
+	}
+
+	@Override
+	public NavEvent initView(TrxInfo info) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
-		
-	
+
+
+
 }
