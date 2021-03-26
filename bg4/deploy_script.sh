435a39480f6d4fb951f4bb25185041481a32615d
premganz <misterprem@gmail.com>
2021-03-26 14:56:31 +0530

    Update deploy_script.sh

--- bg4/deploy_script.sh
+++ bg4/deploy_script.sh
@@ -8,7 +8,7 @@ cd bg4
 if grep --quiet Y deploy.flag; then  
 	echo 'text found'
 	mvn clean install -P fatjar
-  java -cp /home/ec2-user/jetty/bg2-master/target/bg2-1.0.0.jar:/home/ec2-user/jetty/bg2-master/src/main/resources/*:/home/ec2-user/jetty/bg2-master/target/lib/* org.spo.ifs.template.Main
+  java -cp /home/ubuntu/leafycampus/bg4-master/target/bg4-1.0.0.jar:/home/ubuntu/leafycampus/bg4-master/src/main/resources/*:/home/ubuntu/leafycampus/bg4-master/target/lib/* org.spo.ifs.template.Main
 else
 	echo 'not found'
 fi
