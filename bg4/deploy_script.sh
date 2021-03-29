18318d977f25c035ce2bd3991f267dec23102c62
premganz <misterprem@gmail.com>
2021-03-29 23:40:55 +0530

    Update deploy_script.sh

--- bg4/deploy_script.sh
+++ bg4/deploy_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 rm -rf date.txt
-datenow=`date +%Y`
-echo $secret$datenow > date.txt
-hash=`sha256sum date.txt`
+datenow=`date +%Y%m%d%H`
+#echo $secret$datenow
+hash=`echo -n $secret$datenow | sha256sum | cut -c1-20`
 echo $hash
-curl https://github.com/premganz/bg4/blog/master/bg4/deploy.flag | grep $hash
-if [ $? == 0 ]  then
-        read -n1 -r -p "Press any key to continue.." key
+curl https://github.com/premganz/bg4/blob/master/bg4/deploy.flag | grep $hash
+if [ $? == 0 ]
+then
+        #read -n1 -r -p "Press any key to continue.." key       
         echo 'found flag'
         cd /home/ubuntu/leafycampus
         rm -rf master.zip
@@ -15,7 +16,9 @@ if [ $? == 0 ]  then
         unzip master.zip
         cd bg4-master/bg4
         mvn clean install -P fatjar
-        java -cp /home/ubuntu/leafycampus/bg4-master/target/bg4-1.0.0.jar:/home/ubuntu/leafycampus/bg4-master/src/main/resources/*:/home/ubuntu/leafycampus/bg4-master/target/lib/* org.spo.ifs.template.Main
-else
-        echo exiting
+        cp ~/data-cms/cms1 ~/data-backup/$datenow/
+        cp /home/ubuntu/leafycampus/bg4-master/bg4/src/main/resources/Schema_resource.xml ~/data-cms/cms1/content/meta/meta-sc
+hema/Schema-cms.xml
+        java -cp /home/ubuntu/leafycampus/bg4-master/bg4/target/bg4-b1.6.2.jar:/home/ubuntu/leafycampus/bg4-master/bg4/src/main/resources/*:/home/ubuntu/leafycampus/bg4-master/bg4/target/lib/* org.spo.ifs.template.Main
 fi
+echo done
