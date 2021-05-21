886c9074261d14804a96725fe102fe7baccbf902
prem <misterprem@gmail.com>
2021-05-22 00:44:34 +0530

    tech articles

---
+++ bg4/src/main/java/org/spo/svc3/trx/pgs/mdl/PageMeta.java
@@ -0,0 +1,17 @@
+package org.spo.svc3.trx.pgs.mdl;
+
+public class PageMeta {
+	private String photoStrip;
+	
+
+	
+	public String getPhotoStrip() {
+		return photoStrip;
+	}
+
+	public void setPhotoStrip(String photoStrip) {
+		this.photoStrip = photoStrip;
+	}
+	
+
+}
